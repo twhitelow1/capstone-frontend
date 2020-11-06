1786a34bbea89ee8593ea92876b73bae6db6bdd4
Todd Whitelow <todd@whitelow.tech>
2020-11-06 01:54:19 -0700

    add checkboxes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,15 @@
   <div class="home">
     <h1>To Do List</h1>
     <a href="?completed=true">Completed</a>
-    <ul class="todo-list">
-      <li v-for="assignment in filteredAssignments" class="todo" :key="`assign-${assignment.id}`">
-        <input class="toggle" type="checkbox" v-model="assignment.completed" />
-        <label @click="showAssignment(assignment)">{{ `${assignment.chore.title} ` }}</label>
-        | Assigned To: {{ assignment.user.first_name }}
-        <p>-----------------------------------------</p>
+    <ul class="assignment-list">
+      <li v-for="assignment in filteredAssignments" class="assignment" :key="`assign-${assignment.id}`">
+        <label class="container">
+          <input class="toggle" type="checkbox" v-model="assignment.completed" />
+          <span class="checkmark"></span>
+        </label>
+        <label @click="showAssignment(assignment)" class="complete-box">
+          Assigned To: {{ assignment.user.first_name }} | Chore: {{ assignment.chore.title }}
+        </label>
       </li>
     </ul>
     <dialog id="assignment-details">
@@ -16,6 +19,7 @@
         <p>Assigned To: {{ currentAssignment.user.first_name }}</p>
         <p>Chore: {{ currentAssignment.chore.title }}</p>
         <p>Date Due: {{ parseISO(currentAssignment.due_date) }}</p>
+        <p>Is Completed?: {{ currentAssignment.completed }}</p>
         <p>Assigned By: {{ currentAssignment.assigner_id }}</p>
         <button>Close</button>
       </form>
@@ -41,17 +45,93 @@
           <button>Close</button>
         </form>
       </dialog>
+      <p>-----------------------------------------</p>
     </div>
   </div>
 </template>
 
 <style>
-li {
-  list-style: none;
-  border: 1px solid rgb(126, 126, 126);
-  background-color: rgb(240, 240, 240);
+.assignment-list {
+  max-width: 600px;
   margin: auto;
-  max-width: 1000px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.assignment {
+  display: flex;
+  margin-bottom: 1em;
+  justify-content: center;
+}
+/* The container */
+.container {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background-color: #2196f3;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+li {
+  list-style-type: none;
 }
 </style>
 
