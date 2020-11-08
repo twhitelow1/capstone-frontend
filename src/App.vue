d7066193c981d1ce5c32e7ff9019a9ff6d882c90
Todd Whitelow <todd@whitelow.tech>
2020-11-08 14:31:05 -0700

    fix nav menu

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <b-navbar toggleable="lg" type="dark">
-        <b-navbar-brand href="/">Home</b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar toggleable="lg" type="dark" variant="dark" sticky="true">
+      <b-navbar-brand href="/">Chore Tracker</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="/">Home</b-nav-item>
-            <b-nav-item href="/about">About</b-nav-item>
-            <b-nav-item href="/signup">Signup</b-nav-item>
-            <b-nav-item href="/login">Login</b-nav-item>
-            <b-nav-item href="/logout">Logout</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="/">Home</b-nav-item>
+          <b-nav-item href="/about">About</b-nav-item>
+          <b-nav-item href="/signup">Signup</b-nav-item>
+          <b-nav-item href="/login">Login</b-nav-item>
+          <b-nav-item href="/logout">Logout</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view />
   </div>
 </template>
