efbc2fb173b1ebf174c8f9ecd79493e1293b4cca
Todd Whitelow <todd@whitelow.tech>
2020-11-09 22:31:36 -0700

    clean up app.vue

--- src/router/index.js
+++ src/router/index.js
@@ -4,17 +4,10 @@ import Signup from "../views/Signup.vue";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Logout from "../views/Logout.vue";
-import AssignmentShow from "../views/AssignmentsShow.vue";
-// import AssignmentsList from "../views/AssignmentsList.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "Home",
-  //   component: Home,
-  // },
   {
     path: "/",
     name: "Home",
@@ -25,22 +18,9 @@ const routes = [
       }
     },
   },
-  {
-    path: "/assignments/:id",
-    name: "assignments-show",
-    component: AssignmentShow,
-  },
   { path: "/signup", name: "signup", component: Signup },
   { path: "/login", name: "login", component: Login },
   { path: "/logout", name: "logout", component: Logout },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
 ];
 
 const router = new VueRouter({
